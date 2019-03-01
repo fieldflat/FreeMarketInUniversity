b95f8472187fc0089490f9efb3341272a91542fe
Tomonori HIRATA <tomonori4565@icloud.com>
2019-03-01 23:11:17 +0900

    Create Home View

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  #
+  # full_title メソッド
+  # タイトルを返す．
+  #
+  def full_title(message='')
+    if message.empty?
+      "FMU"
+    else
+      "#{message} | FMU"
+    end
+  end
+
 end
