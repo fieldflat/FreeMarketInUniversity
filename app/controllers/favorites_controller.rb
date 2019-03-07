92dce4b70ead0ab325c2b494d00810ee5b948b20
Tomonori HIRATA <tomonori4565@icloud.com>
2019-03-07 21:07:14 +0900

    refactor about favorite system

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,20 @@ class FavoritesController < ApplicationController
 
   def create
     Favorite.create!(user_id: current_user.id, micropost_id: params[:micropost_id])
+    # respond_to do |format|
+    #   format.html { redirect_to request.referrer || root_url }
+    #   format.js
+    # end
     redirect_to request.referrer || root_url
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to request.referrer || root_url }
+    #   format.js
+    # end
     redirect_to request.referrer || root_url
   end
 
