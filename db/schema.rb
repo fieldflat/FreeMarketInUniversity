9e270f627b2daed0d0d9e20d1c3fd27d00526594
Tomonori HIRATA <tomonori4565@icloud.com>
2019-03-08 15:10:07 +0900

    add user_image column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190308034000) do
+ActiveRecord::Schema.define(version: 20190308060930) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20190308034000) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "user_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
