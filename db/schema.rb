ff1809c5829f2acd95a1ead309c384c62a381b0e
Tomonori HIRATA <tomonori4565@icloud.com>
2019-03-11 16:19:33 +0900

    create Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190310054839) do
+ActiveRecord::Schema.define(version: 20190311071857) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20190310054839) do
     t.index ["micropost_id"], name: "index_favorites_on_micropost_id"
     t.index ["user_id", "micropost_id"], name: "index_favorites_on_user_id_and_micropost_id", unique: true
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "room_id"
+    t.integer "from_id"
+    t.integer "to_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
   end
 
   create_table "microposts", force: :cascade do |t|
